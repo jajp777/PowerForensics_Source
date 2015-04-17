135406e31cab1cab7d9b85826afef44b8f0cd210
Jared Atkinson <jared@invoke-ir.com>
2015-04-16 20:39:25 -0500

    Added C# Source code to Repo

---
+++ Invoke-IR.PowerForensics/InvokeIR/PowerForensics/NTFS/LogFile/Cmdlet/Get-LogFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InvokeIR.PowerForensics.Cmdlets
+{
+    class Get_LogFile
+    {
+    }
+}
---
+++ Invoke-IR.PowerForensics/InvokeIR/PowerForensics/NTFS/MFT/Attributes/EA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InvokeIR.PowerForensics.NTFS
+{
+    class EA : Attr
+    {
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Constructors
+
+        #endregion Constructors
+
+    }
+}
